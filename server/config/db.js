484f310347088cd357e5a888449b6506852ab8e0
Tanya <ttruong2811@gmail.com>
2023-06-19 22:29:14 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contact-list"
+    //"URI": "mongodb://localhost/contact-list"
+    "URI": "mongodb+srv://ttruong2811:yoiI77DiKrqJAeAe@clustercomp229.rm4l9ya.mongodb.net/contact-list?retryWrites=true&w=majority"
 }
